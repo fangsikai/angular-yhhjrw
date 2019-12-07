95dcf09640a2280554da44e587d68fd33ed8e0f1
fangsikai <15920372641@139.com>
2019-12-07 14:54:52 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular fang test ';
+  name = 'Angular fang test ,this is a good ide,fangsikai   say lover';
 }
